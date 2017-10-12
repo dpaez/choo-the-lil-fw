d510227933d34fd3f9a24e458ad15ccc27ec9ddf
Diego Paez <diego.paez@despegar.com>
2017-10-12 16:46:46 -0300

    Update: custom toReact fn using create-react-class

---
+++ demos/nano-example/react-example/src/toReact.js
@@ -0,0 +1,37 @@
+var assert = require('assert');
+var createReactClass = require('create-react-class');
+
+module.exports = toReact
+
+function toReact (Component, react) {
+  assert.equal(typeof Component, 'function', 'nanocomponent-adapters/react: component should be type function')
+    //assert.equal(typeof react, 'object', 'nanocomponent-adapters/react: react should be type object')
+
+  var clx = createReactClass({
+    getInitialState: function () {
+      this.node = null
+      this.comp = new Component()
+      return {}
+    },
+    shouldComponentUpdate: function (nextProps) {
+      return false
+    },
+    componentWillReceiveProps: function (nwProps) {
+      if (this.comp.element) this.comp.render({ ...nwProps })
+    },
+    componentDidMount: function () {
+      if (!this.comp.element) {
+        var el = this.comp.render({ ...this.props })
+        this.node.appendChild(el)
+      }
+    },
+    setRef: function (_node) {
+      this.node = _node
+    },
+    render: function () {
+      return react.createElement('div', { ref: this.setRef })
+    }
+  })
+
+  return clx
+}

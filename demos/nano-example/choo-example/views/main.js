d424f9251b63bdd770c506bb1b9edbb27c321b84
Diego Paez <diego.paez@despegar.com>
2017-10-12 16:46:06 -0300

    Update: choo nanoexample

--- demos/nano-example/choo-example/views/main.js
+++ demos/nano-example/choo-example/views/main.js
@@ -1,4 +1,6 @@
 var html = require('choo/html')
+var universal = require('universal-components');
+var button = new universal.Button();
 
 var TITLE = '🚂🚋🚋'
 
@@ -9,8 +11,12 @@ function view (state, emit) {
   return html`
     <body class="sans-serif">
       <h1 class="f-headline pa3 pa4-ns">
-        Choo choo!
+       Nanocomponents example
       </h1>
+      <h3 class="f2 pa3 pa4-ns mid-gray"> >>> Choo Project</h3>
+	  <main class="center tc">
+	    ${button.render('pink')}
+      </main>
     </body>
   `
 }

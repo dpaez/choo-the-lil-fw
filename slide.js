b270e896e304a370956e305077e765c865202033
Diego Paez <paez.di@gmail.com>
2017-10-31 01:02:40 -0300

    Update: add notes support + improve style

--- slide.js
+++ slide.js
@@ -1,6 +1,21 @@
 const html = require('choo/html');
+const css = require('sheetify');
 
-const directionalButtons = (emit) => {
+let showGoto = false;
+
+const menuStyle = css`
+    :host .menu {
+        min-height: 0;
+        max-height: 0;
+        transition: max-height 0.4s;
+    }
+
+    :host .menu--open {
+        max-height: 25rem;
+    }
+`
+
+const directionalButtons = (state, emit) => {
     const movefw = () => {
         emit('choo-slides:fw');
     }
@@ -9,14 +24,65 @@ const directionalButtons = (emit) => {
         emit('choo-slides:bw');
     }
 
+    const toggleGoto = () => {
+        showGoto = !showGoto;
+        emit('render');
+    }
+
+    const gotoItem = (title, idx) => {
+        const gotoSlide = () => {
+            showGoto = false;
+            emit('choo-slides:goto', idx);
+        }
+        return html`
+            <li onclick=${gotoSlide} class="lh-copy pa2 pointer">${title}</li>
+        `
+    }
+
     return html`
-        <section class='absolute bottom-1 right-2'>
-            <button onclick=${movebw} class='pointer grow-large bn no-underline bg-transparent hover-white inline-flex items-center pa3'>
-                <span class='w1 '>◀</span>
+        <section class='${menuStyle} w-50 w-40-m w-auto-ns overflow-y-auto absolute tr bottom-1 right-2'>
+            <button onclick=${movebw} class='outline-transparent pointer grow-large bn no-underline bg-transparent hover-white inline-flex items-center pa3'>
+                <span class='w1'>◀</span>
             </button>
-            <button onclick=${movefw} class='pointer grow-large bn no-underline bg-transparent hover-white inline-flex items-center pa3'>
+            <button onclick=${toggleGoto} class='outline-transparent pointer grow-large bn no-underline bg-transparent hover-white inline-flex items-center pa3'>
+                <span class='w1'>❖ </span>
+            </button>
+            <button onclick=${movefw} class='outline-transparent pointer grow-large bn no-underline bg-transparent hover-white inline-flex items-center pa3'>
                 <span class='w1 '>▶</span>
             </button>
+            <aside class="overflow-x-hidden  menu ${ showGoto ? "menu--open" : ""} db-l w-100 tc mv0 f3 fw3 f5-l bg-black-80 white-90">
+                <div class="pb2 ph4-l ph3 ph5-xl tl measure-wide">
+                    <ul class="list">
+                        ${state.presentation.slides.map((slide, idx) => {
+                            return gotoItem(slide.title, idx)
+                        })}
+                    </ul>
+                </div>
+            </aside>
+        </section>
+    `;
+}
+
+const notes = (children, emit) => {
+    // Note (dk): notes can be turned into somekind of HOC
+    // if that its the case then we can move this out of here
+    // making things smaller.
+
+    if (!children.notes) return '';
+
+    const toogleNotes = () => {
+        children.on = !children.on;
+        emit('render');
+    }
+
+    return html`
+        <section class="${menuStyle} w-50 w-40-m w-auto-ns overflow-y-auto absolute items-center bottom-1 left-2 justify-center">
+            <button class="outline-transparent pointer f5 no-underline black bg-animate bg-transparent hover-white inline-flex items-center pa3 ba b--transparent border-box mr4" onclick="${toogleNotes}"> Notas ${children.on ? '▼': '▲' } </button>
+            <aside class="overflow-x-hidden menu ${children.on ? "menu--open" : ""} db-l w-100 tc mv0 f3 fw3 f5-l bg-black-80 white-90">
+                <div class="pb2 ph4-l ph3 ph5-xl tl measure-wide">
+                    ${ children.notes ? children.notes() : ""}
+                </div>
+            </aside>
         </section>
     `;
 }
@@ -53,7 +119,8 @@ const Slide = children => (state, emit) => {
                     ${children(state)}
                 </div>
             </article>
-            ${directionalButtons(emit)}
+            ${directionalButtons(state, emit)}
+            ${notes(children, emit)}
         </body>
     `;
 }
